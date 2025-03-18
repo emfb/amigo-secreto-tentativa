add9b0345d7dd0d4c858c1cb47067b1fa4abb2b3
emfb <eferreira107@gmail.com>
2025-03-18 18:02:26 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+const inputAmigo = document.getElementById("amigo");
+const listaAmigos = [];
+const ulListaAmigos = document.getElementById("listaAmigos");
+const ulResultado = document.getElementById("resultado");
+
+function adicionarAmigo() {
+    if(inputAmigo.value == ""){
+        alert("Escreva um nome!");
+    }
+    listaAmigos.push(inputAmigo.value);
+    ulListaAmigos.innerHTML += `<li>${inputAmigo.value}</li>`;
+
+ 
+};
+
+function sortearAmigo(){
+    const random = Math.floor(Math.random() * listaAmigos.length);
+    const amigoSecreto = listaAmigos[random];
+    ulResultado.innerHTML = `<li>O Amigo secreto é: ${amigoSecreto}</li>`;
+
+};
